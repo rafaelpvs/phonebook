7c490f83a474809e1e1f804a86232f1250ade008
Rafael Pinto <rafael12wq@gmail.com>
2021-03-13 15:25:46 -0300

    Create models and its migrations

---
+++ database/migrations/2021_03_13_182111_add_contact_id_column_to_phones_tables.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddContactIdColumnToPhonesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('phones', function (Blueprint $table) {
+            $table->integer('contact_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('phones', function (Blueprint $table) {
+            $table->dropColumn('contact_id');
+        });
+    }
+}
---
+++ database/migrations/2021_03_13_182130_add_contact_id_column_to_emails_tables.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddContactIdColumnToEmailsTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('emails', function (Blueprint $table) {
+            $table->integer('contact_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('emails', function (Blueprint $table) {
+            $table->dropColumn('contact_id');
+        });
+    }
+}
